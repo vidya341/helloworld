293b228bb128eaeea2dcf0ffda766cf6047eacbe
vidya <vidya@amberflux.com>
2023-07-12 22:41:15 +0530

    adding two numbers

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+        int a=10;
+        int b = 10;
+        System.out.println(a+b);
+    }
+}
